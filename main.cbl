1a4c673580f84d1b45c66f09f08ee7d3c8716f75
Cherkesky <guy@cherkesky.com>
2020-04-25 21:49:44 -0500

    working on rnd number

--- main.cbl
+++ main.cbl
@@ -7,6 +7,12 @@
             DATA DIVISION.
               *> working storage defines variables
               WORKING-STORAGE SECTION.
+              01 CALC PIC 9(3).
+              01 RND PIC 9(3).
+              01 CURRENT PIC 9(3).
+              01 C PIC 9(1).
+              01 R PIC 9(3).
+
               01 USER-INPUT.
                  05 USER-NAME PIC A(10).
                  05 QUESTION-1 PIC 9(3).
@@ -39,6 +45,7 @@
               DISPLAY "               -- (oo) --                   ".
               DISPLAY "                 //||\\                    ".
               DISPLAY "                                          ".
+              DISPLAY "                                          ".
 
               MOVE "HAVE A SNACK" TO MORNING-ACTIVITY(1).
               MOVE "BINGE NETFLIX" TO MORNING-ACTIVITY(2).
@@ -54,11 +61,7 @@
 
               DISPLAY "What is your name?".
               ACCEPT USER-NAME.
-              display "Hello, " USER-NAME.
-              DISPLAY "What is your name?".
-              ACCEPT USER-NAME.
-              DISPLAY "Hello, " USER-NAME.
-
+              DISPLAY "                                          ".
               DISPLAY "I'll help you create today's quarantine agenda."
               DISPLAY "Just answer a few questions to get started."
 
@@ -72,25 +75,25 @@
               ACCEPT QUESTION-1.
               DISPLAY " ".
               
-              DISPLAY "On a scale of 1 - 638596, how much do you miss".
-              DISPLAY "brunch?".
-              ACCEPT QUESTION-2.    
-              DISPLAY " ".
+              *> DISPLAY "On a scale of 1 - 638596, how much do you miss".
+              *> DISPLAY "brunch?".
+              *> ACCEPT QUESTION-2.    
+              *> DISPLAY " ".
               
-              DISPLAY "Great, last question:".
-              DISPLAY "Where will you go for your first post-social". 
-              DISPLAY "distancing destination?".
-              DISPLAY "     1 - Salon. Hair, nails, the works.".
-              DISPLAY "     2 - The tattoo shop for a commemorative 'I". 
-              DISPLAY "         survived quarantine and all I got was" 
-              DISPLAY "         this lousy tattoo' tattoo".
-              DISPLAY "     3 - I'm leaving on a jet plane. Don't know". 
-              DISPLAY "         when I'll be back again.".
-              DISPLAY "     4 - No, OMG, stop, David".
-              DISPLAY "     5 - My front porch".
-              ACCEPT QUESTION-3.
+              *> DISPLAY "Great, last question:".
+              *> DISPLAY "Where will you go for your first post-social". 
+              *> DISPLAY "distancing destination?".
+              *> DISPLAY "     1 - Salon. Hair, nails, the works.".
+              *> DISPLAY "     2 - The tattoo shop for a commemorative 'I". 
+              *> DISPLAY "         survived quarantine and all I got was" 
+              *> DISPLAY "         this lousy tattoo' tattoo".
+              *> DISPLAY "     3 - I'm leaving on a jet plane. Don't know". 
+              *> DISPLAY "         when I'll be back again.".
+              *> DISPLAY "     4 - No, OMG, stop, David".
+              *> DISPLAY "     5 - My front porch".
+              *> ACCEPT QUESTION-3.
               
+           
 
-            
             *> end our program
             STOP RUN.
