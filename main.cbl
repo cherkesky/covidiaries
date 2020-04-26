4350dac5ba2ef2674d0d499f06798f99b5396701
Caty Chapman <caty@cherkesky.com>
2020-04-25 20:59:30 -0500

    Added text for questions

--- main.cbl
+++ main.cbl
@@ -28,19 +28,41 @@
               DISPLAY "|     |_ |       | |     | |   | |       | ".
               DISPLAY "|_______||_______|  |___|  |___| |______| ".
               
-              MOVE "HAVE A SNACK" TO ACTIVITY-NAME(1).
-              MOVE "BINGE NETFLIX" TO ACTIVITY-NAME(2).
-              MOVE "EXERCISE. HAHA JUST KIDDING" TO ACTIVITY-NAME(3).
+              DISPLAY "What is your name?".
+              ACCEPT USER-NAME.
+              DISPLAY "Hello, " USER-NAME.
+
+              DISPLAY "I'll help you create today's quarantine agenda."
+              DISPLAY "Just answer a few questions to get started."
+
               
-              DISPLAY "What is your name?".
-              ACCEPT USER-NAME.
-              display "Hello, " USER-NAME.
-              DISPLAY "What is your name?".
-              ACCEPT USER-NAME.
-              display "Hello, " USER-NAME.      
-              DISPLAY "What is your name?".
-              ACCEPT USER-NAME.
-              display "Hello, " USER-NAME.
+              DISPLAY "First, how much do you like quarantine?".
+              DISPLAY "     1 - It's not much of a change from my".
+              DISPLAY "         regular routine".
+              DISPLAY "     2 - I want to bust through the walls like ".
+              DISPLAY "         the Kool-Aid guy.".
+              DISPLAY "     3 - Ew, David".
+              ACCEPT QUESTION-1.
+              DISPLAY " ".
+              
+              DISPLAY "On a scale of 1 - 638596, how much do you miss".
+              DISPLAY "brunch?".
+              ACCEPT QUESTION-2.    
+              DISPLAY " ".
+              
+              DISPLAY "Great, last question:".
+              DISPLAY "Where will you go for your first post-social". 
+              DISPLAY "distancing destination?".
+              DISPLAY "     1 - Salon. Hair, nails, the works.".
+              DISPLAY "     2 - The tattoo shop for a commemorative 'I". 
+              DISPLAY "         survived quarantine and all I got was" 
+              DISPLAY "         this lousy tattoo' tattoo".
+              DISPLAY "     3 - I'm leaving on a jet plane. Don't know". 
+              DISPLAY "         when I'll be back again.".
+              DISPLAY "     4 - No, OMG, stop, David".
+              DISPLAY "     5 - My front porch".
+              ACCEPT QUESTION-3.
+              
 
             
             *> end our program
