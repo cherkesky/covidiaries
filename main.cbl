df4dc1cee331a80173e94d266430f7ed8fa14981
Cherkesky <guy@cherkesky.com>
2020-04-25 20:37:44 -0500

    added arrays and user input

--- main.cbl
+++ main.cbl
@@ -7,22 +7,19 @@
             DATA DIVISION.
               *> working storage defines variables
               WORKING-STORAGE SECTION.
-              01 NAME-VAR PIC A(5) VALUE 'WORLD'.
               01 USER-INPUT.
                  05 USER-NAME PIC A(10).
-                 05 IN-1 PIC 9(3).
-                 05 IN-2 PIC 9(3).
-                 05 IN-2 PIC 9(3).
-              01 ACTIVITIES-TABLE.
-                 05 ACTIVITY-NAME PIC X(30) OCCURS 3 TIMES.
-            
+                 05 QUESTION-1 PIC 9(3).
+                 05 QUESTION-2 PIC 9(3).
+                 05 QUESTION-3 PIC 9(3).
+              01 MORNING-TABLE.
+                 05 MORNING-ACTIVITY PIC X(30) OCCURS 3 TIMES.
+              01 NOON-TABLE.
+                 05 NOON-ACTIVITY PIC X(30) OCCURS 3 TIMES.  
+              01 EVENING-TABLE.
+                 05 EVENING-ACTIVITY PIC X(30) OCCURS 3 TIMES.    
             PROCEDURE DIVISION.
-              *> print a string
-              DISPLAY "HELLO " NAME-VAR.
 
-              MOVE "HAVE A SNACK" TO ACTIVITY-NAME(1).
-              MOVE "BINGE NETFLIX" TO ACTIVITY-NAME(2).
-              MOVE "EXERCISE. HAHA JUST KIDDING" TO ACTIVITY-NAME(3).
               DISPLAY ' _______  _______  __   __  ___   ______   '.
               DISPLAY "|       ||       ||  | |  ||   | |      | ".
               DISPLAY "|       ||   _   ||  |_|  ||   | |  _    |".
@@ -30,10 +27,21 @@
               DISPLAY "|      _||  |_|  ||       ||   | | |_|   | ".
               DISPLAY "|     |_ |       | |     | |   | |       | ".
               DISPLAY "|_______||_______|  |___|  |___| |______| ".
-
+              
+              MOVE "HAVE A SNACK" TO ACTIVITY-NAME(1).
+              MOVE "BINGE NETFLIX" TO ACTIVITY-NAME(2).
+              MOVE "EXERCISE. HAHA JUST KIDDING" TO ACTIVITY-NAME(3).
+              
               DISPLAY "What is your name?".
               ACCEPT USER-NAME.
               display "Hello, " USER-NAME.
+              DISPLAY "What is your name?".
+              ACCEPT USER-NAME.
+              display "Hello, " USER-NAME.      
+              DISPLAY "What is your name?".
+              ACCEPT USER-NAME.
+              display "Hello, " USER-NAME.
+
             
             *> end our program
             STOP RUN.
